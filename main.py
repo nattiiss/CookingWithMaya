e465ed55cd6188b4c8e34cf15f6b3d4285d25c12
nattiiss <127318939+nattiiss@users.noreply.github.com>
2023-03-08 17:35:21 +0000

    added dynamic data and API route

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+Cakes = [{
+  'id': 1,
+  'title': 'cake1',
+  'recipe': 'recipe 1'
+}, {
+  'id': 2,
+  'title': 'cake2',
+  'recipe': 'recipe 2'
+}, {
+  'id': 3,
+  'title': 'cake3',
+  'recipe': 'recipe 3'
+}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('index.html')
+  return render_template('index.html', cakes=Cakes)
+
+
+@app.route("/api/cakes")
+def list_cake():
+  return jsonify(Cakes)
 
 
 if __name__ == "__main__":
