b669ff36e76bf8f580e1576db17d31957ee3ce44
nattiiss <127318939+nattiiss@users.noreply.github.com>
2023-03-13 21:22:19 +0000

    New Changes

--- main.py
+++ main.py
@@ -13,9 +13,19 @@ def recipes():
   return render_template('recipes.html')
 
 
+@app.route('/recipe_1/')
+def recipe_1():
+  return render_template('recipe_1.html')
+
+
 @app.route('/about/')
 def about():
   return render_template('about.html')
+
+
+@app.route('/contact/')
+def contact():
+  return render_template('contact.html')
 
 
 if __name__ == "__main__":
